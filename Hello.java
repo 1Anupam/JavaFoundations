d6c96f415ab27ad8590c854c9ee3c2fc93ebc603
Anupam Sushil <anupam.sushil1@gmail.com>
2024-02-21 21:01:49 -0500

    hello world in Java

---
+++ Hello.java
@@ -0,0 +1,8 @@
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
+
+// maximum 1 top level public class which name must match file name - for jvm
+
